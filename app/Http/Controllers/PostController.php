1b930b1ecd8fcd0f07bef999faedddb5f694c483
Luis Armando AA <luis.aguayo@shadesdemexico.com>
2025-01-23 22:18:17 -0600

    FEAT: se agrega la funcion de autenticar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        dd('desde muro');
+        dd(auth()->user());
     }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -34,6 +34,16 @@ class RegisterController extends Controller
             'password' => $request->password
         ]);
 
+        // Autenticar usuario
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //     'password'=>$request->password
+        // ]);
+
+
+        // Otra forma de autenticar
+        auth()->attempt($request->only(['email', 'password']));
+
         // Redireccionar al usuario
         return redirect()->route('post.index');
     }
