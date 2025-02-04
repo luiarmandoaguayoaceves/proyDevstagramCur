8b06dee2b25ece1e85cb8fe53d2b7d70cf968c66
Luis Armando AA <luis.aguayo@shadesdemexico.com>
2025-02-03 19:49:09 -0600

    FEAT: Login creado con ruta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
             </h1>
 
             <nav class="flex gap-2 items-center">
-                <a class="font-bold uppercase text-gray-600" href="#">Login</a>
+                <a class="font-bold uppercase text-gray-600" href="{{ route('login')}}">Login</a>
                 <a class="font-bold uppercase text-gray-600" href={{ route('register') }}>Crear cuenta</a>
             </nav>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('principal');
@@ -10,5 +11,10 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+
 
 Route::get('/muro', [PostController::class, 'index'])->name('post.index');
