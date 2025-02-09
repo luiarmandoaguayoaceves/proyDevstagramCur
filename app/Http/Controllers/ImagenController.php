93ccac73a5ab1f5f060b49b61249352d0acebcd5
Luis Armando AA <luis.aguayo@shadesdemexico.com>
2025-02-09 15:28:24 -0600

    FEAT: Se crea la ruta y el controlador para guarda la imagen en el servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
 
 class ImagenController extends Controller
 {
-    public function  store(Request $request) {
+    public function  store(Request $request)
+    {
         $imagen = $request->file('file');
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+        $manager = new ImageManager(Driver::class);
+        $image = $manager->read($imagen);
+        $image->cover(1000, 1000);
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        $imagePath = public_path('uploads') . '/' .$nombreImagen;
+        $image->save($imagePath);
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
